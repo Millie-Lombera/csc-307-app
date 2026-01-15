9d2b2c417f05f88b068da6b758060cce3c33410d
Millie-Lombera <melomber@calpoly.edu>
2026-01-14 23:07:40 -0800

    Form Data Created

--- packages/react-frontend/src/MyApp.jsx
+++ packages/react-frontend/src/MyApp.jsx
@@ -1,25 +1,9 @@
 import React, { useState } from "react";
 import Table from "./Table";
+import Form from "./Form";
 
 function MyApp() {
-  const [characters, setCharaters] = useState([
-    {
-      name: "Charlie",
-      job: "Janitor"
-    } ,
-    {
-      name: "Mac",
-      job: "Bouncer"
-    },
-    {
-      name: "Dee",
-      job: "Aspring actress"
-    },
-    {
-      name: "Dennis",
-      job: "Bartender"
-    }
-  ]);
+  const [characters, setCharaters] = useState([]);
   
   function removeOneCharacter(index){
     const updated = characters.filter((character, i) => {
