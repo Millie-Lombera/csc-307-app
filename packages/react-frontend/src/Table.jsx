6f545721496f3064c885ffa78d71d913594776b2
Millie-Lombera <melomber@calpoly.edu>
2026-01-14 21:30:45 -0800

    Added Table component & resolved git issue

--- packages/react-frontend/src/Table.jsx
+++ packages/react-frontend/src/Table.jsx
@@ -1,34 +1,47 @@
 import React from "react";
 
-function Table() {
+//Table is a custom made HTML table, that we created as a React Componenet
+//React Components names: Ensure to capatalize to differenciate them between regular HTML elements.
+function TableHeader() {
   return (
-    <table>
-      <thead>
-        <tr>
-          <th>Name</th>
-          <th>Job</th>
-        </tr>
-      </thead>
-      <tbody>
-        <tr>
-          <td>Charlie</td>
-          <td>Janitor</td>
-        </tr>
-        <tr>
-          <td>Mac</td>
-          <td>Bouncer</td>
-        </tr>
-        <tr>
-          <td>Dee</td>
-          <td>Aspiring actress</td>
-        </tr>
-        <tr>
-          <td>Dennis</td>
-          <td>Bartender</td>
-        </tr>
-      </tbody>
-    </table>
+    <thead>
+      <tr>
+        <th>Name</th>
+        <th>Job</th>
+      </tr>
+    </thead>
   );
 }
 
+function TableBody() {
+  return (
+    <tbody>
+      <tr>
+        <td>Charlie</td>
+        <td>Janitor</td>
+      </tr>
+      <tr>
+        <td>Mac</td>
+        <td>Bouncer</td>
+      </tr>
+      <tr>
+        <td>Dee</td>
+        <td>Aspiring actress</td>
+      </tr>
+      <tr>
+        <td>Dennis</td>
+        <td>Bartender</td>
+      </tr>
+    </tbody>
+  );
+}
+
+function Table() {
+  return (
+    <table>
+      <TableHeader />
+      <TableBody />
+    </table>
+  );
+}
 export default Table;
