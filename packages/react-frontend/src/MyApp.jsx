fb5121758dfc7565aeda0bdc2539e8a3f1c391af
Millie-Lombera <melomber@calpoly.edu>
2026-01-14 22:57:22 -0800

    Fully Working Table w/ Button to Remove

--- packages/react-frontend/src/MyApp.jsx
+++ packages/react-frontend/src/MyApp.jsx
@@ -1,10 +1,41 @@
-import React from "react";
+import React, { useState } from "react";
 import Table from "./Table";
+
 function MyApp() {
-    return (
-      <div className = "container">
-          <Table/>
-      </div>
+  const [characters, setCharaters] = useState([
+    {
+      name: "Charlie",
+      job: "Janitor"
+    } ,
+    {
+      name: "Mac",
+      job: "Bouncer"
+    },
+    {
+      name: "Dee",
+      job: "Aspring actress"
+    },
+    {
+      name: "Dennis",
+      job: "Bartender"
+    }
+  ]);
+  
+  function removeOneCharacter(index){
+    const updated = characters.filter((character, i) => {
+      return i != index;
+    });
+    setCharaters(updated);
+  }
+
+  return (
+    <div className = "container">
+        <Table 
+          characterData = {characters}
+          removeCharacter = {removeOneCharacter}
+        />
+    </div>
     );
   }
   export default MyApp; //Allows MyApp compinent to be imported into other componenets or files
+
