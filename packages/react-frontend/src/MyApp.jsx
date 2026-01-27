b0e650bb7f6a789d28feb0f5026d906e1042af25
Millie-Lombera <melomber@calpoly.edu>
2026-01-27 00:03:11 -0800

    Frontend PUSH

--- packages/react-frontend/src/MyApp.jsx
+++ packages/react-frontend/src/MyApp.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import Table from "./Table";
 import Form from "./Form";
 
@@ -6,14 +6,64 @@ function MyApp() {
   const [characters, setCharacters] = useState([]);
   
   function removeOneCharacter(index){
-    const updated = characters.filter((character, i) => {
-      return i != index;
-    });
-    setCharacters(updated);
+    deleteUser(id)
+      .then((response) => {
+        if (response.status === 204) {
+          const updated = characters.filter((c) => c.id !== id);
+          setCharacters(updated);
+        } else if (response.status === 404) {
+          console.log("Resource not found.");
+        }
+      })
+      .catch((error) => {
+        console.log(error);
+      });
   }
 
+  function fetchUsers() {
+    const promise = fetch("http://localhost:8000/users");
+    return promise;
+  }
+
+  useEffect(() => {
+    fetchUsers()
+      .then((res) => res.json())
+      .then((json) => setCharacters(json["users_list"]))
+      .catch((error) => {
+        console.log(error);
+      });
+  }, []);
+
+  function postUser(person) {
+    const promise = fetch("Http://localhost:8000/users", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(person),
+    });
+  
+    return promise;
+  }
+  
   function updateList(person) {
-    setCharacters([...characters, person]);
+    postUser(person)
+      .then((response) => {
+        if (response.status !== 201) {
+          throw new Error("Expected 201 Created");
+        }
+        return response.json();
+      })
+      .then((newUser) => setCharacters([...characters, newUser]))
+      .catch((error) => {
+        console.log(error);
+      });
+  }
+
+  function deleteUser(id) {
+    return fetch(`http://localhost:8000/users/${id}`, {
+      method: "DELETE",
+    });
   }
 
   return (
